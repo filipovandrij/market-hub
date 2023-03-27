326650e2a973ea8ff9cf15eecbf633d0b6108a2a
Filipov <andrijfilipov73@gmail.com>
2023-03-27 19:17:02 +0300

    instal react router

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+
 type Props = {};
 const App = (props: Props) => {
-  return <div>Start to work</div>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+      </Routes>
+      <Footer />
+    </>
+  );
 };
 export default App;
